c2dbe748419c38361dc6c470dddb3424d4b5d360
jenybo <86452879+JenyBo@users.noreply.github.com>
2024-01-20 23:39:54 +0700

    update_mainScreen

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <MainScreen/>
-  <CopyRight/>
+  <main-screen v-if="statusMatch === 'default'" @onStart="onHandleBeforeStart()"/>
+  <interactive-screen v-if="statusMatch === 'match'"/>
+  <copy-right/>
 </template>
 
 <script>
 import MainScreen from "./components/MainScreen.vue";
 import CopyRight from "./components/CopyRight.vue";
-// import InteractiveScreen from "./components/InteractiveScreen.vue";
+import InteractiveScreen from "./components/InteractiveScreen.vue";
 // import ResultScreen from "./components/ResultScreen.vue";
 
 export default {
   name: "App",
-  components: { MainScreen,CopyRight },
+  data(){
+    return {
+      statusMatch: "default",
+    }
+  },
+  components: { MainScreen,CopyRight,InteractiveScreen },
+  methods: {
+    onHandleBeforeStart() {
+      this.statusMatch = "match";
+    }
+  }
 };
 </script>
 
--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -3,18 +3,28 @@
     <h1>Poke Memories</h1>
     <h3>Select mode to start game</h3>
     <div class="action">
-      <button>
+      <button @click="onStart(16)">
         <span>4*4</span>
         <span>Easy</span>
       </button>
-      <button>
+      <button @click="onStart(36)">
         <span>6*6</span>
         <span>Medium</span>
       </button>
-      <button>
+      <button @click="onStart(64)">
         <span>8*8</span>
         <span>Hard</span>
       </button>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    onStart(totalOfBlocks) {
+      this.$emit("onStart", { totalOfBlocks });
+    }
+  }
+}
+</script>
